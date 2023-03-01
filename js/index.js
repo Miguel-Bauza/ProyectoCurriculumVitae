0e2bf62aa7ab8a3c38aeee77a9adfd143ced9cd9
Miguel Angel Bauza <prof.miguel.bauza@gmail.com>
2023-03-01 18:51:46 -0300

    Inicialización del Proyecto Curriculum Vitae

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('enviar').addEventListener('click', function(){
+  console.log("En breve estaremos contactandonos con Usted!!!");
+  alert("En breve estaremos contactandonos con Usted!!!");
+});
